d62cbc4136fdc736731556b64d06cb079b53ca4d
luis escobar <luisescobar74628191@gmail.com>
2025-07-09 09:56:33 -0500

    listo el web de grupo, horarios y sedes

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,13 @@
 
 use App\Http\Controllers\DocenteController;
 use App\Http\Controllers\EstudianteController;
-use App\Http\Controllers\GruposController;
+use App\Http\Controllers\GrupoController;
+use App\Http\Controllers\HorarioController;
 use App\Http\Controllers\MateriaController;
 use App\Http\Controllers\MateriaDocenteController;
 use App\Http\Controllers\MatriculaController;
 use App\Http\Controllers\ProgramaController;
+use App\Http\Controllers\SedeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,6 +21,8 @@ Route::resource('programas', ProgramaController::class);
 Route::resource('materias_docente', MateriaDocenteController::class);
 Route::resource('matriculas', MatriculaController::class);
 Route::resource('grupos', GrupoController::class);
+Route::resource('horarios',HorarioController::class);
+Route::resource('sedes', SedeController::class);
 
 Route::get('/prueba', function () {
     return 'Ruta funcionando';
